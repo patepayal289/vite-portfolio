890fad85d492e08efb7ba4353fdd97fed8101835
patepayal289 <payu34444@gmail.com>
2025-11-15 11:28:39 +0530

    Updated build files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-portfolio/', // EXACTLY your repo name
+  base: '/',   // IMPORTANT for Vercel
   plugins: [react()],
 });
